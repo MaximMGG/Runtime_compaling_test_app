9d3c1b70f6edaf7c899ae98cb9a0d17105de4e45
maximhrunenko <maxhrunenko@gmail.com>
2024-01-12 11:11:15 +0200

    write lib setup func

--- src/profile.c
+++ src/profile.c
@@ -4,8 +4,18 @@
 
 #define Werror_len 3
 static const char Werror[][20] = {"-Wall", "-Werror", "-Wexta"};
+static const char Fsanitize[][64] = {
+                                    "-fsanitize=address", 
+                                    "-fsanitize=address", 
+                                    "-fsanitize=kernel-address",
+                                    "-fsanitize=thread",
+                                    "-fsanitize=pointer-compare"
+                                    };
+
+
 
 static void print_Werror() {
+    puts("Suppresss Warnings: enter f.e. 1, 2, 5 or 1, 2 or 2");
     for(int i = 0; i < Werror_len; i++) {
         printf("%d -> %s\n", i + 1, Werror[i]);        
     }
@@ -36,6 +46,40 @@ static int setup_warning(C_setup *setup) {
     return 0;
 }
 
+static char **map_lib(char **n_libs, int size) {
+    char **map_lib = (char **) malloc(sizeof(char *) * size);
+
+    for(int i = 0; i < size; i++) {
+        map_lib[i] = (char *) malloc(strlen(n_libs[i]) + 2);
+        map_lib[i][0] = '-';
+        map_lib[i][1] = 'l';
+        strcpy(&map_lib[i][2], n_libs[i]);
+        free(n_libs[i]);
+    }
+    free(n_libs);
+
+    return map_lib;
+}
+
+static int setup_libs(C_setup *setup) {
+    puts("Enter libs using in your project f.e. using libcurl just enter curl if libncurses \
+            just ncurses");
+    char temp[128];
+    fgets(temp, 128, stdin);
+    int size = 0;
+    char **libs_c = str_split(temp, ',', &size);
+    libs_c = map_lib(libs_c, size);
+    setup->libs = (libs *) malloc(sizeof(libs *));
+    for(int i = 0; i < size; i++) {
+        setup->libs->lib[i] = (char *) malloc(sizeof(libs_c[i]));
+        strcpy(setup->libs->lib[i], libs_c[i]);
+    }
+    setup->libs->len = size;
+
+    return 0;
+}
+
+
 
 //TODO write this func for libs, sanitaze
 C_setup *profile_create() {
@@ -46,6 +90,7 @@ C_setup *profile_create() {
         free(setup);
         profile_create();
     }
+    setup_libs(setup);
 
     return setup;
 }
